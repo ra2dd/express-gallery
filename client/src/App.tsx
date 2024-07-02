c3523a243a86da1ff15bcfc67ff16fc60f26823c
ra2dd <radoslawpryw@gmail.com>
2024-07-02 16:16:58 +0200

    frontend - add basic routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import Art from './components/Art'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Art />
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/art">Art Pieces</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<p>Home</p>} />
+          <Route path="/art" element={<Art />} />
+          <Route path="/category" element={<p>Categories</p>} />
+          <Route path="*" element={<p>Not Found</p>} />
+        </Routes>
+      </Router>
+
+      <footer>
+        2024
+      </footer>
     </> 
   )
 }
